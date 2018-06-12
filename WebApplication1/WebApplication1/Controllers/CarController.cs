7edfc1541e3313c8ce0deff884720e9932b6a128
cr0hszard <alessandro.gasperini93@gmail.com>
2018-06-12 02:15:42 +0200

    Added Carcontroller(Controller folder) and the Car class(Model folder)

---
+++ WebApplication1/WebApplication1/Controllers/CarController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CarController : ApiController
+    {
+        // GET: api/Car
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Car1", "Car2" };
+        }
+
+        // GET: api/Car/5
+        public string Get(int id)
+        {
+            Car Panda = new Car();
+            Panda.Brand = "FIAT";
+
+            return "Some car";
+        }
+
+        // POST: api/Car
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Car/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Car/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{   //Here we give the car class properties
+    public class Car
+    {
+        public string Brand { get; set; }
+        public string Type { get; set; }
+        public int ProductionYear { get; set; }
+        public int FuelCapacity { get; set; } // in liters
+        public int FuelCompsumption { get; set; }//in liters/100Km
+    }
+}
