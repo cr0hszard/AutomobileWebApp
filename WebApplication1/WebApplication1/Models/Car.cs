54a22359fb86d68f2449d1c9eba83b60888ca153
cr0hszard <alessandro.gasperini93@gmail.com>
2018-06-17 17:07:33 +0200

    minor formatting changes

--- WebApplication1/WebApplication1/Models/Car.cs
+++ WebApplication1/WebApplication1/Models/Car.cs
@@ -26,7 +26,7 @@ namespace WebApplication1.Models
 {  
     public class Car
     {
-        private static Car[] Cars = PopulateArray(20); //static Array of cars
+        private static Car[] Cars = PopulateArray(500); //static Array of cars
 
         //The Counter is used to count the instances of objects and to set a different Id for every car in the constructor
         public static int Counter = 0;
@@ -76,20 +76,18 @@ namespace WebApplication1.Models
         {
             String[] NameArray = { "Alfa", "Peugeot", "Skoda", "BMW", "Audi", "Lamborghini", "Opel", "Seat", "Citroen", "Lada" };//Array of names
 
-            int lowestProductionYear = 2000;//lowest year of production
+            int lowestProductionYear = 1994;//lowest year of production
 
             //Randoms we will use to select random numbers for years and for the index of the NameArray
             Random r1 = new Random();
-            Random r2 = new Random();
-
+            
             //we will create a dummy array the same size of the Cars[] array and populate the array with it 
             Car[] Array = new Car[length];
-
             for (int i = 0; i < length; i++)
             {
                 Array[i] = new Car(
                     NameArray[(int)(r1.NextDouble() * 10)],
-                    lowestProductionYear + (int)(r2.NextDouble() * 18)
+                    lowestProductionYear + (int)(r1.NextDouble() * 24)
                     );
             }
             return Array;
